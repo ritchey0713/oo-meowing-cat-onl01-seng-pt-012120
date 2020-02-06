f66c0b358336d0eb806ce4cd3ad32012fabb4f82
David Ritchey <ritchey0713@gmail.com>
2020-02-06 23:08:22 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,7 +1,7 @@
 class Cat 
   attr_accessor :cat_name
   
-  def meow 
+  def cat_meow 
     puts "meow!"
   end 
 end 
@@ -9,7 +9,9 @@ end
 class Dog 
   attr_accessor :dog_name
   
-  
+  def dog_bark 
+    puts "bark!"
+  end 
 end 
 
 
